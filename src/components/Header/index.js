0ebbc7a0993fa87974083338e3c31893aff33319
Andreszd <contretaa@gmail.com>
2020-10-20 21:31:13 -0400

    Header agregado a laging page Home

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUpload, faUserCircle } from '@fortawesome/free-solid-svg-icons'
+
 import '../../scss/blocks/Header.scss'
+import '../../scss/blocks/nav-menu.scss'
 const Header = () => {
     return ( 
         <header className="header">
@@ -9,10 +13,14 @@ const Header = () => {
             <nav className="header__menu">
                 <ul className="nav-menu">
                     <li className="nav-menu__item">
-                        <a href="#">Upload</a>
+                        <a href="#" className="nav-menu__link" >
+                           <FontAwesomeIcon icon={ faUpload } className="nav-menu__icon"/>
+                            Upload</a>
                     </li>
                     <li className="nav-menu__item">
-                        <a href="#">User</a>
+                        <a href="#" className="nav-menu__link nav-menu__link--background">
+                            <FontAwesomeIcon icon={ faUserCircle } className="nav-menu__icon"/>
+                        </a>
                     </li>
                 </ul>
             </nav>
